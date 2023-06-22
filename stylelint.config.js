407cf1d0260feef206b51e5c028b075cb57f2b44
kirillsakun <kirillsakun03@gmail.com>
2023-06-22 13:19:01 +0200

    Feature (Linters): Add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+	extends: ['stylelint-config-recommended-scss', 'stylelint-config-idiomatic-order'],
+	plugins: [
+		'stylelint-scss',
+		'stylelint-color-format',
+		'stylelint-high-performance-animation',
+		'stylelint-declaration-block-no-ignored-properties',
+		'stylelint-use-logical-spec',
+		'stylelint-prettier',
+	],
+	rules: {
+		'prettier/prettier': true,
+		'selector-class-pattern': false,
+		'scss/at-function-pattern': false,
+		'max-nesting-depth': [
+			4,
+			{
+				ignore: ['pseudo-classes'],
+				ignoreAtRules: ['if', 'else', 'for'],
+			},
+		],
+		'value-no-vendor-prefix': [
+			true,
+			{
+				ignoreValues: ['box'],
+			},
+		],
+		'order/properties-alphabetical-order': false,
+		'scss/at-import-partial-extension': false,
+		'scss/at-import-partial-extension-blacklist': false,
+		'color-named': 'never',
+		'color-format/format': {
+			format: 'hsl',
+		},
+		'plugin/no-low-performance-animation-properties': [true, { ignore: 'paint-properties' }],
+		'plugin/declaration-block-no-ignored-properties': true,
+		'liberty/use-logical-spec': true,
+	},
+};
