219a9ed8c1bfa8d4ed15e9b9f70c68e5be45afc4
kirillsakun <kirillsakun03@gmail.com>
2023-06-26 09:52:56 +0200

    Refactor (Utilities): Move Typography example to separate component

---
+++ src/stories/1.Typography.stories.js
@@ -0,0 +1,8 @@
+import Typography from './componetns/Typography.vue';
+
+export default {
+	title: '1. Typography',
+	component: Typography,
+};
+
+export const Default = {};
--- src/stories/componetns/Typography.vue
+++ src/stories/componetns/Typography.vue
@@ -1,6 +1,5 @@
-const Typography = {
-	template: `
-	<div style="display: flex; flex-direction: column; gap: 1rem;">
+<template>
+	<div class="typography">
 		<div class="heading heading--lg font-weight-800">Heading LG (.heading.heading--lg.font-weight-800)</div>
 		<div class="heading heading--lg">Heading LG (.heading.heading--lg)</div>
 		<div class="heading heading--md">Heading MD (.heading.heading--md)</div>
@@ -14,12 +13,12 @@ const Typography = {
 		<div class="text text--sm">Small text (.text.text--sm)</div>
 		<div class="text text--xs font-weight-700">Extra small text (.text.text--xs.font-weight-700)</div>
 	</div>
-`,
-};
+</template>
 
-export default {
-	title: '1. Typography',
-	component: Typography,
-};
-
-export const Default = {};
+<style scoped lang="scss">
+.typography {
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+}
+</style>
