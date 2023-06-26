5d75eb8bdc1aa84d5f31bb5d6a70123a1073366c
kirillsakun <kirillsakun03@gmail.com>
2023-06-26 15:40:00 +0200

    Update (Utilities): Improve readability

--- src/stories/componetns/StoriesColors.vue
+++ src/stories/componetns/StoriesColors.vue
@@ -1,41 +1,40 @@
 <script setup>
-import { reactive } from 'vue';
+import BaseDivider from '../../components/BaseDivider.vue';
+import BaseSection from '../../components/BaseSection.vue';
+import BaseContainer from '../../components/BaseContainer.vue';
 
-const colorGroups = reactive(
-	new Map([
-		['Base', ['primary', 'secondary', 'tertiary']],
-		['Text', ['text-primary', 'text-secondary']],
-		['Background', ['background-primary', 'background-secondary', 'background-tertiary']],
-		['Success', ['success-700', 'success-500', 'success-300', 'success-100', 'success-50']],
-		['Warning', ['warning-700', 'warning-300', 'warning-100', 'warning-50', 'warning-25']],
-		['Error', ['error-700', 'error-500', 'error-300', 'error-100', 'error-50', 'error-25']],
-		['Accent 1', ['accent-1-text', 'accent-1-hover', 'accent-1-disabled', 'accent-1-background']],
-		['Fallback', ['fallback-text', 'fallback-hover', 'fallback-disabled', 'fallback-background']],
-	])
-);
+const colorGroups = new Map([
+	['Base', ['primary', 'secondary', 'tertiary']],
+	['Text', ['text-primary', 'text-secondary']],
+	['Background', ['background-primary', 'background-secondary', 'background-tertiary']],
+	['Success', ['success-700', 'success-500', 'success-300', 'success-100', 'success-50']],
+	['Warning', ['warning-700', 'warning-300', 'warning-100', 'warning-50', 'warning-25']],
+	['Error', ['error-700', 'error-500', 'error-300', 'error-100', 'error-50', 'error-25']],
+	['Accent 1', ['accent-1-text', 'accent-1-hover', 'accent-1-disabled', 'accent-1-background']],
+	['Fallback', ['fallback-text', 'fallback-hover', 'fallback-disabled', 'fallback-background']],
+]);
 
-const generateColorValue = (name) => `var(--db-components-color-${name})`;
+const generateColorName = (name) => `--db-components-color-${name}`;
 const generateReadableName = (name) => name.replaceAll('-', ' ');
 </script>
 
 <template>
-	<ul class="color-groups">
-		<li
-			v-for="colorGroupKey in colorGroups.keys()"
-			:key="colorGroupKey"
-			class="color-group"
-		>
-			<h2>{{ colorGroupKey }}</h2>
+	<BaseSection
+		v-for="groupName in colorGroups.keys()"
+		:key="groupName"
+	>
+		<BaseContainer>
+			<h2>{{ groupName }}</h2>
 			<ul class="colors-cards">
 				<li
-					v-for="name in colorGroups.get(colorGroupKey)"
-					:key="`${colorGroupKey}-${name}`"
+					v-for="name in colorGroups.get(groupName)"
+					:key="`${groupName}-${name}`"
 					class="color-card"
 				>
 					<div
 						class="color-card__color"
 						:style="{
-							'background-color': generateColorValue(name),
+							'background-color': `var(${generateColorName(name)})`,
 						}"
 					/>
 					<div class="color-card__body">
@@ -43,13 +42,13 @@ const generateReadableName = (name) => name.replaceAll('-', ' ');
 							{{ generateReadableName(name) }}
 						</h3>
 						<div class="text">
-							{{ generateColorValue(name) }}
+							{{ generateColorName(name) }}
 						</div>
 					</div>
 				</li>
 			</ul>
-		</li>
-	</ul>
+		</BaseContainer>
+	</BaseSection>
 </template>
 
 <style scoped lang="scss">
@@ -72,10 +71,10 @@ ul {
 	border-radius: 0.5rem;
 	box-shadow: 0 4px 6px -2px rgba(16, 24, 40, 0.03), 0px 12px 16px -4px rgba(16, 24, 40, 0.08);
 	overflow: hidden;
-	width: 20rem;
+	width: 15rem;
 	&__color {
 		display: block;
-		aspect-ratio: 4/1;
+		aspect-ratio: 2/1;
 	}
 	&__body {
 		padding-block: 0.5em 0.75em;
--- src/stories/componetns/StoriesTypography.vue
+++ src/stories/componetns/StoriesTypography.vue
@@ -1,20 +1,35 @@
 <template>
-	<div class="typography">
-		<div class="heading heading--lg font-weight-800">Heading LG (.heading.heading--lg.font-weight-800)</div>
-		<div class="heading heading--lg">Heading LG (.heading.heading--lg)</div>
-		<div class="heading heading--md">Heading MD (.heading.heading--md)</div>
-		<div class="heading heading--sm">Heading SM (.heading.heading--sm)</div>
-		<div class="heading heading--sm font-weight-400">Heading SM (.heading.heading--sm.font-weight-400)</div>
-		<div class="text font-weight-700">Primary text (.text.font-weight-700)</div>
-		<div class="text font-weight-600">Primary text (.text.font-weight-600)</div>
-		<div class="text font-weight-500">Secondary text (.text.font-weight-500)</div>
-		<div class="text font-weight-400">Secondary text (.text.font-weight-400)</div>
-		<div class="text text--sm font-weight-500">Small text (.text.text--sm.font-weight-500)</div>
-		<div class="text text--sm">Small text (.text.text--sm)</div>
-		<div class="text text--xs font-weight-700">Extra small text (.text.text--xs.font-weight-700)</div>
-	</div>
+	<BaseSection>
+		<BaseContainer>
+			<div class="typography">
+				<div class="heading heading--lg font-weight-800">Heading LG (.heading.heading--lg.font-weight-800)</div>
+				<div class="heading heading--lg">Heading LG (.heading.heading--lg)</div>
+				<BaseDivider />
+				<div class="heading heading--md">Heading MD (.heading.heading--md)</div>
+				<BaseDivider />
+				<div class="heading heading--sm">Heading SM (.heading.heading--sm)</div>
+				<div class="heading heading--sm font-weight-400">Heading SM (.heading.heading--sm.font-weight-400)</div>
+				<BaseDivider />
+				<div class="text font-weight-700">Primary text (.text.font-weight-700)</div>
+				<div class="text font-weight-600">Primary text (.text.font-weight-600)</div>
+				<BaseDivider />
+				<div class="text font-weight-500">Secondary text (.text.font-weight-500)</div>
+				<div class="text font-weight-400">Secondary text (.text.font-weight-400)</div>
+				<BaseDivider />
+				<div class="text text--sm font-weight-500">Small text (.text.text--sm.font-weight-500)</div>
+				<div class="text text--sm">Small text (.text.text--sm)</div>
+				<BaseDivider />
+				<div class="text text--xs font-weight-700">Extra small text (.text.text--xs.font-weight-700)</div>
+			</div>
+		</BaseContainer>
+	</BaseSection>
 </template>
 
+<script setup>
+import BaseDivider from '../../components/BaseDivider.vue';
+import BaseSection from '../../components/BaseSection.vue';
+import BaseContainer from '../../components/BaseContainer.vue';
+</script>
 <style scoped lang="scss">
 .typography {
 	display: flex;
