d67e0e154e48cef3fd53fc80d7d8c3b45d374a1d
kirillsakun <kirillsakun03@gmail.com>
2023-06-28 14:02:02 +0200

    Feature (Utilities): Add palette generation if css color

--- src/composables/colors.js
+++ src/composables/colors.js
@@ -1,12 +1,23 @@
 import { computed, toValue } from 'vue';
 
+const MIX_PERCENTS = new Map([
+	['text', 100],
+	['hover', 70],
+	['disabled', 50],
+	['background', 10],
+]);
+
 export const useColors = (name, options) => {
-	const { prefix = 'db', postfixes = ['text', 'hover', 'disabled', 'background'] } = options || {};
+	const { prefix = 'db', postfixes = Array.from(MIX_PERCENTS.keys()) } = options || {};
 	const isCssColor = computed(() => CSS.supports('color', toValue(name)));
+
+	console.log(postfixes);
 
 	const generateColor = (postfix) =>
 		isCssColor.value
-			? toValue(name)
+			? `color-mix(in srgb, ${toValue(name)} ${MIX_PERCENTS.get(
+					postfix
+			  )}%, var(--db-components-color-background-primary))`
 			: `var(--${prefix}-color-${toValue(name)}-${postfix}, var(--db-components-color-${toValue(
 					name
 			  )}-${postfix}, var(--db-components-color-fallback-${postfix})))`;
