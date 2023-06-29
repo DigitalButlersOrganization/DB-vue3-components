e4087909a1a2efe6066c9c8c8cc5f7e0a48df005
kirillsakun <kirillsakun03@gmail.com>
2023-06-29 16:46:04 +0200

    Feature (Stories): Add TestPlayground story
    Use to see if slots render as you want. Storybook slots kinda stupid ☹️

---
+++ src/stories/TestPlayground.stories.js
@@ -0,0 +1,10 @@
+import TestPlayground from './componetns/TestPlayground.vue';
+
+export default {
+	title: 'Test/Playground',
+	component: TestPlayground,
+};
+
+export const Test = {
+	args: {},
+};
---
+++ src/stories/componetns/TestPlayground.vue
@@ -0,0 +1,26 @@
+<script setup>
+import VAvatar from '../../components/VAvatar.vue';
+</script>
+
+<template>
+	<VAvatar>
+		<template #icon> icon </template>
+		<template #badge> 1 </template>
+	</VAvatar>
+</template>
+
+<style lang="scss" scoped>
+.theme--dark {
+	button {
+		color: green;
+	}
+}
+
+button {
+	color: blueviolet;
+
+	span {
+		color: red;
+	}
+}
+</style>
