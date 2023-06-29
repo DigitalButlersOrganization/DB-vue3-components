60002834bf5d24de2020f9f71dac06ac9a72b05d
kirillsakun <kirillsakun03@gmail.com>
2023-06-29 17:34:12 +0200

    Update (Linters): Add airbnb eslint config, bump us dependencies versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,35 @@
 module.exports = {
-	extends: ['prettier', 'plugin:vue/vue3-recommended'],
+	extends: ['airbnb-base', 'plugin:vue/vue3-recommended', 'prettier'],
 	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': [1],
+		'no-implicit-coercion': [
+			2,
+			{
+				boolean: true,
+				number: true,
+				string: true,
+				disallowTemplateShorthand: true,
+				allow: [],
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				pathGroups: [
+					{
+						pattern: './*',
+						group: 'sibling',
+					},
+				],
+			},
+		],
+		'import/prefer-default-export': 0,
 		'vue/html-indent': [0],
 		'vue/html-self-closing': [
 			1,
