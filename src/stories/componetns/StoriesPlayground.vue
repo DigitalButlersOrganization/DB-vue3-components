5fc4de71ae5566f6ec42a772290561c53c9431a1
kirillsakun <kirillsakun03@gmail.com>
2023-07-04 17:13:09 +0200

    Feature (Utilities): Replace black with currentColor for all the icons

--- src/stories/componetns/StoriesPlayground.vue
+++ src/stories/componetns/StoriesPlayground.vue
@@ -1,22 +1,62 @@
 <script setup>
+import { ref } from 'vue';
+
 import DbAvatar from '../../components/DbAvatar.vue';
+import DbBaseDivider from '../../components/DbBaseDivider.vue';
+import DbCheckbox from '../../components/DbCheckbox.vue';
 import DbUserInfo from '../../components/DbUserInfo.vue';
+
+const checkboxValue = ref(true);
+const checkboxGroupValue = ref(['1', '2']);
 </script>
 
 <template>
-<!--	<DbAvatar>-->
-<!--		<template #icon> icon </template>-->
-<!--		<template #badge> 1 </template>-->
-<!--	</DbAvatar>-->
-	<div class="user-info-wrapper">
-		<DbUserInfo
-			full-name="John Doe Junior The Third"
-			description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, quas."
+	<div class="playground">
+		<DbAvatar>
+			<template #icon> icon </template>
+			<template #badge> 1 </template>
+		</DbAvatar>
+		<DbBaseDivider />
+		<div class="user-info-wrapper">
+			<DbUserInfo
+				full-name="John Doe Junior The Third"
+				description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, quas."
+			/>
+		</div>
+		<DbBaseDivider />
+		{{ checkboxValue }}
+		<DbCheckbox
+			v-model="checkboxValue"
+			value="test"
 		/>
+		<DbBaseDivider />
+		{{ checkboxGroupValue }}
+		<DbCheckbox
+			v-model="checkboxGroupValue"
+			value="1"
+		>
+			<template #default> 1 </template>
+		</DbCheckbox>
+		<DbCheckbox
+			v-model="checkboxGroupValue"
+			value="2"
+		>
+			<template #default> 2 </template>
+		</DbCheckbox>
+		<DbCheckbox
+			v-model="checkboxGroupValue"
+			value="3"
+		>
+			<template #default> 3 </template>
+		</DbCheckbox>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+.playground :deep(.divider) {
+	margin-block: 1rem !important;
+}
+
 .user-info-wrapper {
 	max-inline-size: 200px;
 }
