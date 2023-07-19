07b67a562eb5ef5d970b5a0745b74ffc3c8fa56f
kirillsakun <kirillsakun03@gmail.com>
2023-07-19 09:33:34 +0200

    Update (Stylelint, packages): Convert color variables to oklch format, update stylelint color rules, bump up dependencies versions
    AI report:
    The entire color scheme has been updated from previous hsl color encoding to new oklch format to enhance color accuracy and perception. Furthermore, a number of packages have been updated to newer versions to keep the project up-to-date with other dependencies. `stylelint-color-format` has been replaced by `stylelint-gamut` for gamut color range linting. Also, updated Storybook and ESlint packages to newer versions.

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: ['stylelint-config-recommended-scss', 'stylelint-config-idiomatic-order'],
 	plugins: [
 		'stylelint-scss',
-		'stylelint-color-format',
+		'stylelint-gamut',
 		'stylelint-high-performance-animation',
 		'stylelint-declaration-block-no-ignored-properties',
 		'stylelint-use-logical-spec',
@@ -40,10 +40,10 @@ module.exports = {
 		'order/properties-alphabetical-order': null,
 		'scss/at-import-partial-extension': null,
 		'scss/at-import-partial-extension-blacklist': null,
-		'color-named': 'never',
-		'color-format/format': {
-			format: 'hsl',
-		},
+		'gamut/color-no-out-gamut-range': true,
+		'function-disallowed-list': ['rgba', 'hsla', 'rgb', 'hsl'],
+		'color-function-notation': 'modern',
+		'color-no-hex': true,
 		'plugin/no-low-performance-animation-properties': [true, { ignore: 'paint-properties' }],
 		'plugin/declaration-block-no-ignored-properties': true,
 		'liberty/use-logical-spec': true,
