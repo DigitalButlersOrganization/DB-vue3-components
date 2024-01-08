349474f5584f05f9099e69f9334afdd22bebcf4a
Vitali Makal <vmakal1989@gmail.com>
2024-01-08 11:29:38 +0300

    Feature(Components): Add DbBrand

---
+++ src/components/DbBrand.vue
@@ -0,0 +1,54 @@
+<script setup>
+import InlineSvg from 'vue-inline-svg';
+
+import { generateIconPath } from '../utilities';
+
+const props = defineProps({
+	tag: {
+		type: String,
+		required: false,
+		default: 'a',
+	},
+	href: {
+		type: String,
+		required: false,
+		default: '/',
+	},
+	src: {
+		type: String,
+		required: false,
+		default: 'solid/logo-360',
+	},
+	alt: {
+		type: String,
+		required: false,
+		default: '360',
+	},
+	isImg: {
+		type: Boolean,
+		required: false,
+		default: false,
+	},
+});
+</script>
+
+<template>
+	<component
+		:is="props.tag"
+		:href="props.href"
+	>
+		<img
+			v-if="props.isImg"
+			:src="props.src"
+			:alt="props.alt"
+			class="avatar__image"
+		/>
+		<InlineSvg
+			v-else
+			:src="generateIconPath(props.src)"
+			aria-label="360"
+		/>
+	</component>
+</template>
+
+<style scoped lang="scss"></style>
